90b2bbb799bde792ed04b2e179c502a698092e5c
muratti32 <muratti32@gmail.com>
2021-08-29 00:08:15 +0300

    fixed build time localhost error

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev
-  ? "http://localhost:3000"
-  : "https://your-server.appspot";
+export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
